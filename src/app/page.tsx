74cf42eecf7c0de7ed9054944498c0de72beb5ee
ziascode <ziayousaf94@gmail.com>
2025-02-04 18:49:40 -0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,74 @@
-import Image from "next/image";
+'use client';
+import './globals.css';
+import { motion } from 'framer-motion';
+import Link from "next/link";
+import { Poppins, Figtree } from 'next/font/google';
+import SlidingCards from './components/slider';
+
+
+
+
+const figtree700 = Figtree({
+  subsets: ["latin"],
+  weight: "700"
+})
+
+
+
+const poppins200 = Poppins({
+  subsets: ["latin"],
+  weight: "400"
+})
+
+
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+   <div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <div className="flex flex-col  h-[160vh] bg-[#ef7c00]  align-bottom bg-gradient-to-b from-orange-500 to-orange-300  "> {/*  */}
+            <div>
+
+              {/*  */}
+
+              <div className='flex flex-col'>
+                  <div className="flex flex-col justify-center text-center font-[Outfit] px-[27vw] pt-24 pb-1">
+                      <motion.h1 className={` ${figtree700.className} font-sans font-bold text-6xl text-gray-900`}
+                      initial={{ opacity: 0, y: 20 }} 
+                      animate={{ opacity: 1, y: 0 }} 
+                      transition={{ duration: 0.8, ease: "easeOut" }}> Support The Ummah This Ramadan</motion.h1>
+
+                      <motion.h1 className={` ${poppins200.className} my-6 font-sans text-xl text-slate-100`}
+                      initial={{ opacity: 0, y: 20 }} 
+                      animate={{ opacity: 1, y: 0 }} 
+                      transition={{ duration: 0.8, ease: "easeOut" }}>Ramadan 2025 is approaching, and it is time to embark on a transformative spiritual journey to a month that is defined by unity, compassion, and selfless giving. Together, we can support families in need, fund educational opportunities, provide food and water, and much more.</motion.h1>
+                  </div>
+                  <div className='flex justify-center mb-20 gap-5'>
+                      <Link href="https://pennyappeal.ca/">
+                      <button className={` ${figtree700.className} shadow-xl mb-0 px-10 py-4 bg-white border-white-10 rounded-full hover:bg-[#042d49] hover:ease-in-out hover:duration-200 hover:text-white`}>DONATE</button>
+                      </Link>
+                      <Link href="https://pennyappeal.ca/">
+                      <button className={` ${figtree700.className} shadow-xl mb-0 px-9 py-4 text-slate-800 border-gray-100 border-2 bg-opacity-10 border-white-10 rounded-full hover:bg-blue-950 hover:bg-opacity-25 hover:border-blue-950 hover:border-opacity-0 hover:text-white hover:ease-in-out hover:duration-200`}>EXPLORE</button>
+                      </Link>
+                  </div>
+                  
+              </div>
+
+              
+                          
+              {/*Slider */}
+              <SlidingCards/>     
+                           
+            
+              </div>
+              
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+
+
+    
+
+   </div>
   );
 }
+
+
