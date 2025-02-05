4b193dbc358266a2b86829108c065130828aa205
ziascode <ziayousaf94@gmail.com>
2025-02-05 13:04:44 -0500

    fifth commit

--- src/app/components/sliderMobile.tsx
+++ src/app/components/sliderMobile.tsx
@@ -6,17 +6,15 @@ const items = [
   { id: 3, title: "Automate Your Ramadan",  target:"", image: "https://pennyappeal.ca/wp-content/uploads/2024/03/hands-praynew.jpg" },
   { id: 4, title: "Gaza Emergency Response", target: "https://pennyappeal.ca/project/gaza-emergency-response/", image: "https://pennyappeal.ca/wp-content/uploads/2024/09/1ynew.jpg" },
   { id: 2, title: "Sponsor an Orphan", target:"https://donate.pennyappeal.ca/program/orphankind/", image: "https://pennyappeal.ca/wp-content/uploads/2024/01/img4.jpg" },
-//   { id: 5, title: "Volunteer Oppurtunities", place:"Get involved at Penny Appeal Canada", target:"https://pennyappeal.ca/about/team-orange/", image: "https://pennyappeal.ca/wp-content/uploads/2024/03/fow9new.jpg" },
 ];
 
 interface CardProps {
     image: string;
     title: string;
-    place: string;
     target: string;
   }
   
-  const Card: React.FC<CardProps> = ({ image, place, title, target}) => (
+  const Card: React.FC<CardProps> = ({ image, title, target}) => (
     <Link href={target}>
     <div
   className="max-w-sm p-7 mx-2 my-5 pt-20 min-w-[21vw] w-[45vw] border-gray-200 rounded-xl shadow-none shadow-slate-50 dark:bg-gray-800 dark:border-gray-700 opacity-95 relative bg-cover bg-center transition-all duration-300 hover:shadow-xl hover:shadow-[0px_0px_0px_5px_#ffffff] hover:scale-[103%]"
@@ -55,7 +53,6 @@ export default function SliderMobile({ className }: { className?: string }) {
             key={index}
             title={item.title}
             image={item.image}
-            place={item.place}
             target={item.target}
           />
         ))}
