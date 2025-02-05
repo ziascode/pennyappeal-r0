584944fafc4cf4efab34c44f0e2e576bc6a440ae
ziascode <ziayousaf94@gmail.com>
2025-02-05 16:03:04 -0500

    seventh commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,17 +35,17 @@ export default function Home() {
 
               <div className='flex flex-col'>
                   <div className="flex flex-col justify-center text-center font-[Outfit] px-6 md:px-[27vw] pt-24 pb-1">
-                      <motion.h1 className={` ${figtree700.className} font-sans font-bold text-6xl text-[#02243d]`}
+                      <motion.h1 className={` ${figtree700.className} font-sans font-bold text-5xl md:text-6xl text-[#02243d]`}
                       initial={{ opacity: 0, y: 20 }} 
                       animate={{ opacity: 1, y: 0 }} 
                       transition={{ duration: 0.8, ease: "easeOut" }}> Prepare For Ramadan</motion.h1>
 
-                      <motion.h1 className={` ${poppins200.className} my-6 font-sans text-xl text-slate-100`}
+                      <motion.h1 className={` ${poppins200.className} my-6 font-sans text-md md:text-xl text-slate-100`}
                       initial={{ opacity: 0, y: 20 }} 
                       animate={{ opacity: 1, y: 0 }} 
                       transition={{ duration: 0.8, ease: "easeOut" }}>Ramadan 2025 is approaching, and it is time to embark on a transformative spiritual journey to a month that is defined by unity, compassion, and selfless giving.</motion.h1>
                   </div>
-                  <div className='flex justify-center mb-20 gap-5'>
+                  <div className='flex justify-center pb-12 md:mb-20 gap-5'>
                       <Link href="https://pennyappeal.ca/">
                       <button className={` ${figtree700.className} shadow-xl mb-0 px-10 py-4 bg-white border-white-10 rounded-full hover:bg-[#042d49] hover:ease-in-out hover:duration-200 hover:text-white`}>DONATE</button>
                       </Link>
