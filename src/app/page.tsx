30c1b213340baa768fbe480f30cb0219db3ade10
ziascode <ziayousaf94@gmail.com>
2025-02-10 12:00:23 -0500

    12th commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,7 +38,7 @@ export default function Home() {
                       <motion.h1 className={` ${figtree700.className} font-sans font-bold text-5xl md:text-6xl text-[#02243d]`}
                       initial={{ opacity: 0, y: 20 }} 
                       animate={{ opacity: 1, y: 0 }} 
-                      transition={{ duration: 0.8, ease: "easeOut" }}> Prepare For Ramadan</motion.h1>
+                      transition={{ duration: 0.8, ease: "easeOut" }}> Get Ready For Ramadan</motion.h1>
 
                       <motion.h1 className={` ${poppins200.className} my-6 font-sans text-md md:text-xl text-slate-100`}
                       initial={{ opacity: 0, y: 20 }} 
@@ -46,7 +46,7 @@ export default function Home() {
                       transition={{ duration: 0.8, ease: "easeOut" }}>Ramadan 2025 is approaching, and it is time to embark on a transformative spiritual journey to a month that is defined by unity, compassion, and selfless giving.</motion.h1>
                   </div>
                   <div className='flex justify-center pb-12 md:mb-20 gap-5'>
-                      <Link href="https://donate.pennyappeal.ca/">
+                      <Link href="https://donate.pennyappeal.ca/" target="_blank" rel="noopener noreferrer">
                       <button className={` ${figtree700.className} shadow-xl mb-0 px-10 py-4 bg-white border-white-10 rounded-full hover:bg-[#042d49] hover:ease-in-out hover:duration-200 hover:text-white`}>DONATE TODAY</button>
                       </Link>
                       
